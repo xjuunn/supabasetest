5fb34bb9d2d0565411257fba3538b69ef91b8d0a
Junhsiun <xjuunn@gmail.com>
2024-10-05 10:02:14 +0800

    test

---
+++ pages/test2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-for="(item,index) in user" :key="index">
+      {{item}}
+  </div>
+</template>
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const user = ref([]);
+onMounted(async()=>{
+  const {data} = await supabase.from('user').select("*")
+  user.value = data;
+
+})
+</script>
+
+<style scoped>
+
+</style>
